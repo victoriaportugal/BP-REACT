3e50f9779b4712679692688409983806d1a4054c
victoriaportugal <victoria.poortugal@gmail.com>
2024-04-29 19:00:18 -0300

    Segundo commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,29 @@
-import Home from './paginas/home/Home';
+import React from 'react';
 import './App.css';
-
+import Home from './pages/home/Home';
+import Navbar from './components/navbar/Navbar';
+import Footer from './components/footer/Footer';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Login from './pages/login/Login';
 
 function App() {
   return (
-   <Home />
-  );
+    <>
+    <BrowserRouter>
+        <Navbar />
+          <div className='min-h-[80vh]'>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/home" element={<Home />} />
+            </Routes>
+          </div>
+          <Footer />
+        </BrowserRouter>
+    </>
+);
 }
-
 export default App;
+    /*As classes são as unidades básicas que você pode usar para aplicar estilos a elementos HTML
+    os componentes geralmente se referem aos elementos React que são estilizados usando as classes do Tailwind CSS
+    */
